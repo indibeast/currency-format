43a7d3e0eadf3fd27025fbc004220c78f170cd2f
Indika G <indika@ceynet.asia>
2015-09-22 19:56:04 +0530

    added license info

--- src/Reader/Reader.php
+++ src/Reader/Reader.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of the indibeast/currency-format library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) Indika Gunasekara <indigun89@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @link https://packagist.org/packages/indibeast/currency-formatter Packagist
+ * @link https://github.com/indibeast/currency-format GitHub
+ */
 namespace Currency\Reader;
 
 use Currency\Exceptions\FileNotFoundException;
